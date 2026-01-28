24159fcd8ff9b27ab2a9e4168eef0bc1c0419338
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2026-01-28 11:16:15 -0300

    feat: Add build script and .gitignore for output and source directories

---
+++ build-deb.sh
@@ -0,0 +1,12 @@
+mkdir source
+mkdir output
+
+cp -r DEBIAN source/
+cp -r usr source/
+cp -r etc source/
+cp -r schemas source/
+
+rm -r source/etc/skel
+rm -r source/etc/vasakos
+
+dpkg-deb --build --root-owner-group source output/vasakos-desktop-settings_0.1.3_all.deb
